2fe3daa498bc48b509225888ef11fc3a4008d76b
Jiyoon <kjytamu@tamu.edu>
2021-09-16 23:22:39 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,212 @@
+// // import logo from './logo.svg';
+// import './App.css';
+// import tamu from './TAM-PrimaryMarkB.svg';
+// // import { S } from 'xmlchars/xml/1.0/ed5';
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         {<img src={tamu} className="App-logo" alt="logo" /> }
+//         <p>
+//           Aggie Routes
+//         </p>
+//         <form>
+//           <div class= "tofrom">
+//         From:
+//         <input type="From" name="From" placeholder="Starting Point">
+//         </input>
+//         <sp></sp>
+//         To:
+//         <input type="To" name="To" placeholder="Destination">
+//           </input>
+//         <button type="button button 1">Go!</button>
+//           </div>
+//         </form>
+//         <p>
+//           <div class= "subtitle">
+//           Popular routes
+//           </div>
+          
+//         </p>
+//         <div class = "container">
+//           <div class="dropdown">
+//           <button class="dropbtn">Zachary</button>
+//           <div class="dropdown-content">
+//             <a href="https://pages.github.tamu.edu/helenmah/HowdyHack.github.io/">White Creek</a>
+//             <a href="https://pages.github.tamu.edu/helenmah/MSCtoZACH/">MSC</a>
+//             <a href="https://pages.github.tamu.edu/helenmah/HowdyHack2021.github.io/">Reed Arena</a>
+//             </div>
+//           </div>
+//           <div class="dropdown">
+//           <button class="dropbtn">MSC</button>
+//           <div class="dropdown-content">
+//             <a href="#">White Creek</a>
+//             <a href="#">Zachary</a>
+//             <a href="#">Reed Arena</a>
+//             </div>
+//           </div>
+//           <div class="dropdown">
+//           <button class="dropbtn">Reed Arena</button>
+//           <div class="dropdown-content">
+//             <a href="#">White Creek</a>
+//             <a href="#">Zachary</a>
+//             <a href="#">MSC</a>
+//             </div>
+//           </div>
+//           <div class="dropdown">
+//           <button class="dropbtn">White Creek</button>
+//           <div class="dropdown-content">
+//             <a href="https://pages.github.tamu.edu/belanjia000/WC2Reed/.">Reed Arena</a>
+//             <a href="#">Zachary</a>
+//             <a href="https://pages.github.tamu.edu/kjytamu/WCtoMSC/">MSC</a>
+//             </div>
+//           </div>
+//         </div>
+//         <div class = "container">
+//           {/* <div class="dropdown">
+//           <button class="dropbtn">Reed Arena</button>
+//           <div class="dropdown-content">
+//             <a href="#">White Creek</a>
+//             <a href="#">Zachary</a>
+//             <a href="#">MSC</a>
+//             </div>
+//           </div>
+//           <div class="dropdown">
+//           <button class="dropbtn">White Creek</button>
+//           <div class="dropdown-content">
+//             <a href="#">Reed Arenak</a>
+//             <a href="#">Zachary</a>
+//             <a href="#">MSC</a>
+//             </div>
+//           </div> */}
+//         </div>
+        
+//         <a
+//           className="App-link"
+//           href="https://transport.tamu.edu/busroutes/"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Aggie Bus Website
+//         </a>
+
+//       </header>
+      
+//     </div>
+    
+
+    
+//   );
+
+// }
+
+// export default App;
+
+// import logo from './logo.svg';
+import './App.css';
+import tamu from './TAM-PrimaryMarkA.svg';
+// import { S } from 'xmlchars/xml/1.0/ed5';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {<img src={tamu} className="App-logo" alt="logo" /> }
+        <p>
+          Aggie Routes
+        </p>
+        <form>
+          <div class= "tofrom">
+        From:
+        <input type="From" name="From" placeholder="Starting Point">
+        </input>
+        <sp></sp>
+        To:
+        <input type="To" name="To" placeholder="Destination">
+          </input>
+        <button type="button button 1">Go!</button>
+          </div>
+        </form>
+        <p>
+          <div class= "subtitle">
+          ♪ Popular routes ♪
+          </div>
+          </p>
+          
+        
+        <div class = "container">
+          <div class="dropdown">
+          <button class="dropbtn">Zachary</button>
+          <div class="dropdown-content">
+            <a href="https://pages.github.tamu.edu/helenmah/HowdyHack.github.io/">White Creek</a>
+            <a href="https://pages.github.tamu.edu/yeonsu-jung/zachtomsc.github.io/">MSC</a>
+            <a href="https://pages.github.tamu.edu/helenmah/HowdyHack2021.github.io/">Reed Arena</a>
+            </div>
+          </div>
+          <div class="dropdown">
+          <button class="dropbtn">MSC</button>
+          <div class="dropdown-content">
+            <a href="https://pages.github.tamu.edu/helenmah/MSCtoWhite/">White Creek</a>
+            <a href="https://pages.github.tamu.edu/helenmah/MSCtoZACH/">Zachary</a>
+            <a href="https://pages.github.tamu.edu/helenmah/MSCtoReed/">Reed Arena</a>
+            </div>
+          </div>
+          <div class="dropdown">
+          <button class="dropbtn">Reed Arena</button>
+          <div class="dropdown-content">
+            <a href=" https://pages.github.tamu.edu/helenmah/ReedtoWhite/">White Creek</a>
+            <a href="https://pages.github.tamu.edu/yeonsu-jung/reedtozach/">Zachary</a>
+            <a href="https://pages.github.tamu.edu/helenmah/ReedtoMSC/">MSC</a>
+            </div>
+          </div>
+          <div class="dropdown">
+          <button class="dropbtn">White Creek</button>
+          <div class="dropdown-content">
+            <a href="https://pages.github.tamu.edu/belanjia000/WC2Reed/.">Reed Arena</a>
+            <a href="https://pages.github.tamu.edu/helenmah/WhitetoZACH/">Zachary</a>
+            <a href="https://pages.github.tamu.edu/kjytamu/WCtoMSC/.">MSC</a>
+            </div>
+          </div>
+        </div>
+        <div class = "container">
+          {/* <div class="dropdown">
+          <button class="dropbtn">Reed Arena</button>
+          <div class="dropdown-content">
+            <a href="#">White Creek</a>
+            <a href="#">Zachary</a>
+            <a href="#">MSC</a>
+            </div>
+          </div>
+          <div class="dropdown">
+          <button class="dropbtn">White Creek</button>
+          <div class="dropdown-content">
+            <a href="#">Reed Arenak</a>
+            <a href="#">Zachary</a>
+            <a href="#">MSC</a>
+            </div>
+          </div> */}
+        </div>
+        
+        <a
+          className="App-link"
+          href="https://transport.tamu.edu/busroutes/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aggie Bus Website
+        </a>
+
+      </header>
+      
+    </div>
+    
+
+    
+  );
+
+}
+
+export default App;
---
+++ src/components/home.js
@@ -0,0 +1,27 @@
+// // import * from 'react';
+
+// const home = () => {
+
+//     return (
+//         <div>
+//             <h1>Hello World!</h1>
+//         </div>
+//     );
+
+// }
+
+// export default home;
+
+
+import React from 'react';
+import map from './Zach-To-White.jpg';
+
+function Home (){
+
+    return (
+    //<img src="https://upload.wikimedia.org/wikipedia/commons/c/c0/Rosaceae_Malus_pumila_Malus_pumila_Var_domestica_Apples_Fuji.jpg" width="200" height="250">
+    <h1>Welcome to the world of Geeks!</h1>
+    );
+}
+  
+export default Home;
